69880dc96a3418590afcd5d777221d57a910b3ea
hari123-ui <157099152+hari123-ui@users.noreply.github.com>
2024-03-11 22:42:28 +0530

    #include<stdio.h> int main() {     int x ;     scanf("%d",&x);     if(x%2==0)     {         printf("not weird");     }     else     {         printf("weird");     }     return 0; }

---
+++ Weird_or_not_Weird.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x ;
+    scanf("%d",&x);
+    if(x%2==0)
+    {
+        printf("not weird");
+    }
+    else
+    {
+        printf("weird");
+    }
+    return 0;
+}
