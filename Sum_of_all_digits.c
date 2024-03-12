b0688129fc266ce14106ce105949950ad4d74da4
hari123-ui <157099152+hari123-ui@users.noreply.github.com>
2024-03-12 21:09:04 +0530

    #include<stdio.h> int main() {     int x,r=0,t=0;     scanf("%d",&x);     while(x != 0)     {         r = x%10;         t = t+r;         x = x/10;     }     printf("%d",t);     return 0; }

--- Sum_of_all_digits.c
+++ Sum_of_all_digits.c
@@ -3,13 +3,12 @@ int main()
 {
     int x,r=0,t=0;
     scanf("%d",&x);
-    while (x != 0)
+    while(x != 0)
     {
         r = x%10;
         t = t+r;
         x = x/10;
-        
     }
     printf("%d",t);
-    
+    return 0;
 }
