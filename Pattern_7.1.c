01168365f829276bb0c4605e75feacd8a8b25371
Hari <157099152+hari123-ui@users.noreply.github.com>
2024-03-17 11:36:42 +0530

    #include<stdio.h> int main() {     int x;     scanf("%d",&x);     for(int i=1;i<=x;i++)     {         for(int s=1;s<i;s++)         {             printf(" ");         }         for(int j=x;j>=i;j--)         {             printf("* ");         }         printf(" ");     }     return 0; }

---
+++ Pattern_7.1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    for(int i=1;i<=x;i++)
+    {
+        for(int s=1;s<i;s++)
+        {
+            printf(" ");
+        }
+        for(int j=x;j>=i;j--)
+        {
+            printf("* ");
+        }
+        printf("
+");
+    }
+    return 0;
+}
