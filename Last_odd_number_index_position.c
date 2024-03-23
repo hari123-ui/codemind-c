e46ca150ba086355d3297c28405b2f384b643029
Hari <157099152+hari123-ui@users.noreply.github.com>
2024-03-23 06:59:23 +0530

    #include<stdio.h> int main() {     int x,z=0;     scanf("%d",&x);     int a[x];     for(int i=0;i<x;i++)     {         scanf("%d",&a[i]);         if(a[i]%2 !=0)         {             z = i;         }     }     printf("%d",z); }

---
+++ Last_odd_number_index_position.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int x,z=0;
+    scanf("%d",&x);
+    int a[x];
+    for(int i=0;i<x;i++)
+    {
+        scanf("%d",&a[i]);
+        if(a[i]%2 !=0)
+        {
+            z = i;
+        }
+    }
+    printf("%d",z);
+}
